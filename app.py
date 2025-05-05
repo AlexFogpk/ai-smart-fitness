820604cbea10a664b987c49098769a8490acc79d
Aleksei Ostapenko <us7476673@gmail.com>
2025-05-05 15:45:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask, request
 import os
 from telegram import Bot, Update
-from telegram.ext import CommandHandler, MessageHandler, filters, Dispatcher
 
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
-
 app = Flask(__name__)
-
 bot = Bot(token=TELEGRAM_TOKEN)
 
 @app.route('/')
@@ -15,26 +12,17 @@ def home():
 
 @app.route('/telegram-webhook', methods=['POST'])
 def telegram_webhook():
-    if request.method == "POST":
-        update = Update.de_json(request.get_json(force=True), bot)
-        dp.process_update(update)
-        return 'ok', 200
+    update = Update.de_json(request.get_json(force=True), bot)
+    chat_id = update.message.chat.id
+    text = update.message.text
 
-def start(update, context):
-    update.message.reply_text('Привет! Это Ai_SmartFitnessBot. Я помогу тебе с фитнесом и питанием!')
-
-def echo(update, context):
-    update.message.reply_text('Я получил от тебя сообщение: ' + update.message.text)
-
-from telegram.ext import Dispatcher
-
-dp = Dispatcher(bot, None, workers=0)
-dp.add_handler(CommandHandler("start", start))
-dp.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-def run():
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # Простая обработка
+    if text == '/start':
+        bot.send_message(chat_id=chat_id, text='Привет! Это Ai_SmartFitnessBot. Я помогу тебе с фитнесом и питанием!')
+    else:
+        bot.send_message(chat_id=chat_id, text='Я получил от тебя сообщение: ' + text)
+    return 'ok', 200
 
 if __name__ == '__main__':
-    run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
