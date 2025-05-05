fc6f5a2ad64448bef07392655f23025eee034c29
Aleksei Ostapenko <us7476673@gmail.com>
2025-05-05 15:21:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ def home():
     return "Hello, this is the Ai_SmartFitnessBot backend!"
 
 if __name__ == '__main__':
+    # Railway передает порт через переменную окружения 'PORT'
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
